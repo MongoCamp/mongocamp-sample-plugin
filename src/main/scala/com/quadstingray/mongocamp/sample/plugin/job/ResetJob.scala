c0cf7430d629963a4097b7c2c187f7084410492a
QuadStingray <github@quadstingray.com>
2022-08-04 06:10:34 +0000

    feat: started resetjob

--- src/main/scala/com/quadstingray/mongocamp/sample/plugin/job/ResetJob.scala
+++ src/main/scala/com/quadstingray/mongocamp/sample/plugin/job/ResetJob.scala
@@ -1,12 +1,24 @@
 package com.quadstingray.mongocamp.sample.plugin.job
 
 import com.typesafe.scalalogging.LazyLogging
+import dev.mongocamp.server.config.ConfigHolder
+import dev.mongocamp.server.database.MongoDatabase
 import org.quartz.{ Job, JobExecutionContext }
 
 class ResetJob extends Job with LazyLogging {
 
   override def execute(context: JobExecutionContext): Unit = {
-    logger.error(s"${this.getClass.getSimpleName} executed")
+    val ignoredDatabases = List("admin", "config", "local")
+    MongoDatabase.databaseProvider.databaseNames.foreach(database => {
+      if (ConfigHolder.dbConnectionDatabase.value.equalsIgnoreCase(database)) {
+        logger.error(s"${database} should cleaned up.")
+      }
+      else {
+        if (!ignoredDatabases.contains(database)) {
+          logger.error(s"${database} should deleted?")
+        }
+      }
+    })
   }
 
 }
