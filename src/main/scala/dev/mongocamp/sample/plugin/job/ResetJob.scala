ee82f269822a914eab7450d2867339f474af67ad
QuadStingray <github@quadstingray.com>
2022-08-17 05:40:58 +0000

    feat: ignored databases and drop other db

--- src/main/scala/dev/mongocamp/sample/plugin/job/ResetJob.scala
+++ src/main/scala/dev/mongocamp/sample/plugin/job/ResetJob.scala
@@ -1,24 +1,26 @@
 package dev.mongocamp.sample.plugin.job
 
 import com.typesafe.scalalogging.LazyLogging
-import dev.mongocamp.server.config.ConfigHolder
+import dev.mongocamp.driver.mongodb._
+import dev.mongocamp.server.config.{ ConfigHelper, ConfigHolder }
 import dev.mongocamp.server.database.MongoDatabase
 import org.quartz.{ Job, JobExecutionContext }
 
 class ResetJob extends Job with LazyLogging {
 
   override def execute(context: JobExecutionContext): Unit = {
-    val ignoredDatabases = List("admin", "config", "local")
+    val ignoredDatabases = ConfigHelper.globalConfigStringList("ignored.database")
     MongoDatabase.databaseProvider.databaseNames.foreach(database => {
       if (ConfigHolder.dbConnectionDatabase.value.equalsIgnoreCase(database)) {
         logger.error(s"${database} should cleaned up.")
       }
       else {
         if (!ignoredDatabases.contains(database)) {
-          logger.error(s"${database} should deleted?")
+          MongoDatabase.databaseProvider.dropDatabase(database).result()
         }
       }
     })
+    ""
   }
 
 }
