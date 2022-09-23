b4c6cc274453fa8f1f54e228ad50e7b51acc1407
QuadStingray <github@quadstingray.com>
2022-09-23 20:29:03 +0200

    feat: Insert default data

--- src/main/scala/dev/mongocamp/sample/plugin/ResetServerPlugin.scala
+++ src/main/scala/dev/mongocamp/sample/plugin/ResetServerPlugin.scala
@@ -1,15 +1,26 @@
 package dev.mongocamp.sample.plugin
 
+import akka.actor.Props
+import dev.mongocamp.sample.plugin.event.listener.UserChangesListener
 import dev.mongocamp.sample.plugin.job.ResetJob
+import dev.mongocamp.server.auth.AuthHolder
 import dev.mongocamp.server.config.ConfigHelper
+import dev.mongocamp.server.event.{Event, EventSystem}
 import dev.mongocamp.server.exception.MongoCampException
 import dev.mongocamp.server.model.JobConfig
-import dev.mongocamp.server.model.auth.UserInformation
+import dev.mongocamp.server.model.auth.{AuthorizedCollectionRequest, Grant, Role, UserInformation}
 import dev.mongocamp.server.plugin.{JobPlugin, ServerPlugin}
 
 class ResetServerPlugin extends ServerPlugin {
 
   override def activate(): Unit = {
+    checkOrSetDefaultValue("rest.admin.user", "admin")
+    checkOrSetDefaultValue("rest.admin.password", "admin")
+    checkOrSetDefaultValue("rest.admin.role.name", "adminRole")
+    checkOrSetDefaultValue("rest.user.user", "user")
+    checkOrSetDefaultValue("rest.user.password", "user")
+    checkOrSetDefaultValue("rest.user.role.name", "userRole")
+
     val jobUser = UserInformation(this.getClass.getSimpleName, "", None, List())
     try {
       val added = JobPlugin.addJob(jobUser, JobConfig("ResetDataJob", classOf[ResetJob].getName, "", "0 0/30 * ? * * *", "SampleData"))
@@ -23,13 +34,48 @@ class ResetServerPlugin extends ServerPlugin {
     }
   }
 
+  private def checkOrSetDefaultValue(configPath: String, defaultValue: String) = {
+    val confString = try{
+      ConfigHelper.globalConfigString(configPath)
+    } catch {
+      case _: Exception => ""
+    }
+    if (confString.trim.equalsIgnoreCase("")) {
+      val systemSettingKey           = configPath.toUpperCase().replace(".", "_")
+      System.setProperty(systemSettingKey, defaultValue)
+    }
+  }
+
 }
 
 object ResetServerPlugin {
 
-  val adminUser: String = ConfigHelper.globalConfigString("rest.admin.user")
-  val adminPwd: String = ConfigHelper.globalConfigString("rest.admin.password")
+  lazy val adminUser: String = ConfigHelper.globalConfigString("rest.admin.user")
+  lazy val adminPwd: String = ConfigHelper.globalConfigString("rest.admin.password")
+  lazy val adminRoleName: String = ConfigHelper.globalConfigString("rest.admin.role.name")
+  lazy val adminRole: Role = Role(
+    adminRoleName,
+    isAdmin = true,
+    List(
+      Grant(AuthorizedCollectionRequest.all, read = true, write = true, administrate = true, Grant.grantTypeCollection),
+      Grant(AuthorizedCollectionRequest.all, read = true, write = true, administrate = true, Grant.grantTypeBucket)
+    )
+  )
 
-  val userUser: String = ConfigHelper.globalConfigString("rest.user.user")
-  val userPwd: String = ConfigHelper.globalConfigString("rest.user.password")
+  lazy val defaultAdminUser: UserInformation = UserInformation(adminUser, AuthHolder.handler.encryptPassword(adminPwd), None, List(adminRole.name))
+
+  lazy val userUser: String = ConfigHelper.globalConfigString("rest.user.user")
+  lazy val userPwd: String = ConfigHelper.globalConfigString("rest.user.password")
+  lazy val userRoleName: String = ConfigHelper.globalConfigString("rest.user.role.name")
+  lazy val userRole: Role = Role(
+    userRoleName,
+    isAdmin = false,
+    List(
+      Grant("users", read = true, write = false, administrate = false, Grant.grantTypeCollection),
+      Grant("pokemon", read = true, write = true, administrate = false, Grant.grantTypeCollection)
+    )
+  )
+
+  lazy val defaultTestUser: UserInformation = UserInformation(userUser, AuthHolder.handler.encryptPassword(userPwd), None, List(userRole.name))
+
 }
--- src/main/scala/dev/mongocamp/sample/plugin/job/ResetJob.scala
+++ src/main/scala/dev/mongocamp/sample/plugin/job/ResetJob.scala
@@ -3,8 +3,9 @@ package dev.mongocamp.sample.plugin.job
 import com.typesafe.scalalogging.LazyLogging
 import dev.mongocamp.driver.mongodb._
 import dev.mongocamp.driver.mongodb.database.DatabaseProvider
+import dev.mongocamp.sample.plugin.ResetServerPlugin
 import dev.mongocamp.server.config.{ConfigHelper, ConfigHolder}
-import dev.mongocamp.server.database.MongoDatabase
+import dev.mongocamp.server.database.{MongoDatabase, RolesDao, UserDao}
 import dev.mongocamp.server.service.SystemFileService
 import org.quartz.{Job, JobExecutionContext}
 
@@ -13,15 +14,21 @@ class ResetJob extends Job with LazyLogging {
   override def execute(context: JobExecutionContext): Unit = {
     val mongoCampPrefix  = ConfigHolder.authCollectionPrefix.value
     val ignoredDatabases = ConfigHelper.globalConfigStringList("database.ignored")
+    val userCollection = s"${mongoCampPrefix}users"
+    val rolesCollection = s"${mongoCampPrefix}roles"
     MongoDatabase.databaseProvider.databaseNames.foreach(database => {
       if (ConfigHolder.dbConnectionDatabase.value.equalsIgnoreCase(database)) {
         MongoDatabase.databaseProvider
           .collectionNames(database)
           .foreach {
-            case s"${mongoCampPrefix}users" =>
-              ""
-            case s"${mongoCampPrefix}roles" =>
-              ""
+            case s: String if s.equalsIgnoreCase(userCollection)  =>
+              MongoDatabase.databaseProvider.collection(s"$database${DatabaseProvider.CollectionSeparator}$userCollection").deleteMany(Map()).result()
+              val userInsert = UserDao().insertMany(List(ResetServerPlugin.defaultAdminUser, ResetServerPlugin.defaultTestUser)).result()
+              userInsert
+            case s: String if s.equalsIgnoreCase(rolesCollection)  =>
+              MongoDatabase.databaseProvider.collection(s"$database${DatabaseProvider.CollectionSeparator}$rolesCollection").deleteMany(Map()).result()
+              val roleInsert = RolesDao().insertMany(List(ResetServerPlugin.userRole, ResetServerPlugin.adminRole)).result()
+              roleInsert
             case s: String if s.startsWith(mongoCampPrefix) =>
               ""
             case s: String =>
@@ -29,10 +36,10 @@ class ResetJob extends Job with LazyLogging {
           }
         case class MapCollectionDao(collectionName: String) extends MongoDAO[Map[String, Any]](MongoDatabase.databaseProvider, collectionName)
 
-        val usersInsertResult = MapCollectionDao("users").insertMany(SystemFileService.readJsonList("sample/users.json")).result()
+        val usersInsertResult   = MapCollectionDao("users").insertMany(SystemFileService.readJsonList("sample/users.json")).result()
         val pokemonInsertResult = MapCollectionDao("pokemon").insertMany(SystemFileService.readJsonList("sample/pokedex.json")).result()
 
-        ""
+        usersInsertResult.wasAcknowledged() && pokemonInsertResult.wasAcknowledged()
       }
       else {
         if (!ignoredDatabases.contains(database)) {
